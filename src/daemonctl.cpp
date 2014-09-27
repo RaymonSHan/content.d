0b61a3777ef048bb4a407e45f22051f5ed00d525
Raymon SHan <quickhorse77@gmail.com>
2014-09-28 00:24:39 +0800

    success in huge page get

--- src/daemonctl.cpp
+++ src/daemonctl.cpp
@@ -11,43 +11,49 @@
 //#include <sys/stat.h> 
 #include <time.h> 
 #include <sys/mman.h>
-//#include "../include/daemonctl.h"
+//#include "../include/daemonctl.h"1
 
 #define SIZE_RAY 64
 
+unsigned long difftime (timespec &t2, timespec &t1) {
+  return ((t2.tv_sec-t1.tv_sec)*1000000000 + (t2.tv_nsec-t1.tv_nsec));
+}
+
 int
 main (int , char **) {
-  struct timespec  ts1, ts2, ts3;
+  struct timespec  ts1, ts2, ts3, ts4;
   char* smallpagestart;
   char* nowstart, *nowend;
 
+  for (int loop = 0; loop < 1280; loop++) {
+    clock_gettime(CLOCK_MONOTONIC, &(ts1));
 
-  clock_gettime(CLOCK_MONOTONIC, &(ts1));
+    smallpagestart = static_cast<char*>
+      (mmap (0, SIZE_RAY << 21, PROT_READ | PROT_WRITE,
+	     MAP_HUGETLB | MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    clock_gettime(CLOCK_MONOTONIC, &(ts2));
 
-  smallpagestart = static_cast<char*>
-    (mmap (0, SIZE_RAY << 22, PROT_READ | PROT_WRITE,
-	   /*MAP_HUGETLB |*/ MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
-  nowstart = smallpagestart;
-  nowend = nowstart + (SIZE_RAY << 22);
-  do {
-    *nowstart = 0x67;
-    nowstart += 4096;
+    nowstart = smallpagestart;
+    nowend = nowstart + (SIZE_RAY << 21);
+    do {
+      *nowstart = 0x67;
+      nowstart += 4096;
+    }
+    while (nowstart < nowend);
+    clock_gettime(CLOCK_MONOTONIC, &(ts3));
+
+    nowstart = smallpagestart + 1;
+    nowend = nowstart + (SIZE_RAY << 21);
+    do {
+      *nowstart = 0x68;
+      nowstart += 4096;
+    }
+    while (nowstart < nowend);
+
+    clock_gettime(CLOCK_MONOTONIC, &(ts4));
+    munmap(smallpagestart, SIZE_RAY << 21);
   }
-  while (nowstart < nowend);
-  clock_gettime(CLOCK_MONOTONIC, &(ts2));
-
-  nowstart = smallpagestart + 1;
-  nowend = nowstart + (SIZE_RAY << 22);
-  do {
-    *nowstart = 0x68;
-    nowstart += 4096;
-  }
-  while (nowstart < nowend);
-
-  clock_gettime(CLOCK_MONOTONIC, &(ts3));
-
-  // printf("%lu,%lu,%lu\n", ts1.tv_nsec, ts2.tv_nsec, ts3.tv_nsec);
-  printf("%lu,%lu\n", ts2.tv_nsec-ts1.tv_nsec, ts3.tv_nsec-ts2.tv_nsec);
+  printf("%x, %7lu,%12lu,%9lu\n", smallpagestart, difftime(ts2,ts1), difftime(ts3,ts2), difftime(ts4,ts3));
 
   return 0;
 }
