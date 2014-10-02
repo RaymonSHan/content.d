478eb83f6a84854df1c7a5a031b7195a081ba4c6
Raymon SHan <quickhorse77@gmail.com>
2014-10-03 00:29:37 +0800

    after test inotify

--- src/daemonctl.cpp
+++ src/daemonctl.cpp
@@ -13,7 +13,9 @@
 #include <fcntl.h>
 #include <time.h> 
 #include <sys/mman.h>
-//#include "../include/daemonctl.h"1
+#include <sys/inotify.h>
+#include <limits.h>    // for NAME_MAX is 255
+//#include "../include/daemonctl.h"
 
 //#define HUGE_PAGE_USED
 
@@ -74,6 +76,70 @@ void perrorexit(const char* s) {
   exit(-1);
 }
 
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+
+//#include "tlpi_hdr.h"
+/* Display information from inotify_event structure */
+static void displayInotifyEvent(struct inotify_event *i)
+{
+  printf("    wd =%2d; ", i->wd);
+  if (i->cookie > 0)
+    printf("cookie =%4d; ", i->cookie);
+
+  printf("mask = ");
+  if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
+  if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+  if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+  if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+  if (i->mask & IN_CREATE)        printf("IN_CREATE ");
+  if (i->mask & IN_DELETE)        printf("IN_DELETE ");
+  if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+  if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
+  if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
+  if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+  if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+  if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+  if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+  if (i->mask & IN_OPEN)          printf("IN_OPEN ");
+  if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+  if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+  printf("\n");
+
+  if (i->len > 0)
+    printf("        name = %s\n", i->name);
+}
+
+int inotifytest(void)
+{
+  int inotifyFd, wd;
+  char buf[BUF_LEN] __attribute__ ((aligned(8)));
+  ssize_t numRead;
+  char *p;
+  struct inotify_event *event;
+
+  inotifyFd = inotify_init();                 /* Create inotify instance */
+  if (inotifyFd == -1) perrorexit("inotify_init");
+
+  wd = inotify_add_watch(inotifyFd, FILEPATH, IN_ALL_EVENTS);
+  if (wd == -1) perrorexit("inotify_add_watch");
+
+  for (;;) {                                  /* Read events forever */
+    numRead = read(inotifyFd, buf, BUF_LEN);
+    if (numRead == 0) perror("read() from inotify fd returned 0!");
+
+    if (numRead == -1) perrorexit("read");
+    printf("Read %ld bytes from inotify fd\n", (long) numRead);
+
+    /* Process all of the events in buffer returned by read() */
+    for (p = buf; p < buf + numRead; ) {
+      event = (struct inotify_event *) p;
+      displayInotifyEvent(event);
+      p += sizeof(struct inotify_event) + event->len;
+    }
+  }
+  exit(EXIT_SUCCESS);
+}
+
 int mmapfile(void) {
   int fd;
   assignStart.addressLong = SEGMENT_START;
@@ -91,7 +157,7 @@ int mmapfile(void) {
   if (result == -1) perrorexit("Error calling lseek()");
 
   result = write(fd, "", 1);
-  if (result == -1) perrorexit("Error calling write()");  //  printf("%s\n", realStart.addressChar);
+  if (result == -1) perrorexit("Error calling write()");
 
   for (int i=0; i<100; i++) {
     printf ("%s\n", (realStart.addressChar));
@@ -228,7 +294,8 @@ int hugePageAlloc () {
 }
 
 int main(int , char **) {
-  mmapfile();
+  inotifytest();
+  // mmapfile();
   //  mapunmap();
   //  madviseDontNeed();
   //  hugePageAlloc();
